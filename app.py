dbde2913f1159f0305785114439fce05476c9028
GusttavoMartins <gustavo10887@gmail.com>
2024-10-23 22:28:09 -0300

    add app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def main ():
     email = st.text_input('E-mail: ')
     data = st.date_input('Data de Venda', datetime.now())
     hora = st.time_input('Hora da Venda', value=datetime.today())
-    valor = st.number_input('Valor da Venda: ', min_value=0.0,  format="%.2f", step= 0.50)
+    valor = st.number_input('Valor da Venda ', min_value=0.0,  format="%.2f", step= 0.50)
     quantidade = st.number_input('Quantidade de Produtos ', min_value = 0, step= 1)
     produto = st.selectbox(' Produto Vendido', ['','Camista', 'Cueca', 'Bermuda'])
     
